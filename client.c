63649fae68c6c9d82970b23d636d401c9f278722
agupte01 <arg17@sfu.ca>
2025-11-20 23:20:42 -0800

    misc

---
+++ client.c
@@ -0,0 +1,210 @@
+#define _POSIX_C_SOURCE 200102L
+#define _GNU_SOURCE
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int convert(uint8_t *buf, ssize_t buf_size, char *str, ssize_t str_size) {
+  if (buf == NULL || str == NULL || buf_size <= 0 ||
+      str_size < (buf_size * 2 + 1)) {
+    return -1;
+  }
+  for (int i = 0; i < buf_size; i++)
+    sprintf(str + i * 2, "%02X", buf[i]);
+  str[buf_size * 2] = '\0';
+  return 0;
+}
+
+typedef struct {
+  int sockfd;
+  char *log_file_path;
+} ReceiverArgs;
+
+void *receiver_thread(void *arg) {
+  ReceiverArgs *params = (ReceiverArgs *)arg;
+  FILE *logfile = fopen(params->log_file_path, "a");
+
+  if (!logfile) {
+    perror("fopen lof file");
+    pthread_exit(NULL);
+  }
+
+  while (1) {
+    uint8_t recv_buf[1024];
+    ssize_t rlen = read(params->sockfd, recv_buf, sizeof(recv_buf));
+    if (rlen <= 0) {
+      printf("server closed connection or error. Exiting\n");
+      fclose(logfile);
+      close(params->sockfd);
+      exit(EXIT_SUCCESS);
+    }
+
+    uint8_t msg_type = recv_buf[0];
+    if (msg_type == 1) {
+      printf("Recieved type 1 (shutdown) from server. Exiting\n");
+      fprintf(logfile, "Received type 1 from server. Exiting\n");
+      fclose(logfile);
+      close(params->sockfd);
+      exit(EXIT_SUCCESS);
+    } else if (msg_type == 0 && rlen >= 7) {
+      uint32_t sender_ip;
+      uint16_t sender_port;
+      memcpy(&sender_ip, recv_buf + 1, sizeof(sender_ip));
+      memcpy(&sender_port, recv_buf + 5, sizeof(sender_port));
+      struct in_addr ip_addr;
+      ip_addr.s_addr = sender_ip;
+      char *ip_str = inet_ntoa(ip_addr);
+      int port = ntohs(sender_port);
+
+      int msg_offset = 7;
+      int msg_len = rlen - msg_offset;
+      char message[1024];
+      memcpy(message, recv_buf + msg_offset, msg_len);
+      message[msg_len] = '\0';
+
+      printf("%-15s%-10u%s", ip_str, port, message);
+      fprintf(logfile, "%-15s%-10u%s", ip_str, port, message);
+      fflush(logfile);
+    }
+  }
+  fclose(logfile);
+  pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 5) {
+    fprintf(stderr,
+            "Usage: %s <IP address> <port number> <# of messages> <log file "
+            "path>\n",
+            argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  char *server_ip = argv[1];
+  int port = atoi(argv[2]);
+  int num_messages = atoi(argv[3]);
+  char *log_file_path = argv[4];
+
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) {
+    perror("socket");
+    exit(EXIT_FAILURE);
+  }
+
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(port);
+
+  if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <= 0) {
+    perror("inet_pton");
+    exit(EXIT_FAILURE);
+  }
+
+  if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) <
+      0) {
+    perror("connect");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Connected to server %s%d\n", server_ip, port);
+
+  ReceiverArgs recv_args = {sockfd, log_file_path};
+  pthread_t recv_tid;
+  if (pthread_create(&recv_tid, NULL, receiver_thread, &recv_args) != 0) {
+    perror("pthread_create");
+    close(sockfd);
+    exit(EXIT_FAILURE);
+  }
+
+  int msg_bytes = 16;
+  uint8_t rand_bytes[msg_bytes];
+  char hex_msg[msg_bytes * 2 + 1];
+
+  for (int i = 0; i < num_messages; i++) {
+    if (getentropy(rand_bytes, msg_bytes) != 0) {
+      perror("getentropy");
+      exit(EXIT_FAILURE);
+    }
+    if (convert(rand_bytes, msg_bytes, hex_msg, sizeof(hex_msg)) != 0) {
+      fprintf(stderr, "Conversion error\n");
+      exit(EXIT_FAILURE);
+    }
+
+    size_t hex_len = strlen(hex_msg);
+    uint8_t send_buf[1 + msg_bytes * 2 + 2];
+    send_buf[0] = 0;
+    memcpy(send_buf + 1, hex_msg, hex_len);
+    send_buf[1 + hex_len] = '\n';
+
+    size_t total_len = 1 + hex_len + 1;
+    ssize_t w = write(sockfd, send_buf, total_len);
+    if (w != total_len) {
+      perror("write");
+      exit(EXIT_FAILURE);
+    }
+    printf("sent hex message %d: %s\n", i + 1, hex_msg);
+  }
+
+  uint8_t type1_msg = 1;
+  if (write(sockfd, &type1_msg, 1) != 1) {
+    perror("write type 1");
+    close(sockfd);
+    exit(EXIT_FAILURE);
+  }
+  printf("Sent type 1 (shutdown) message to server.\n");
+
+  pthread_join(recv_tid, NULL);
+
+  /*FILE *logfile = fopen(log_file_path, "a");
+  if (!logfile) {
+    perror("fopen log file");
+    exit(EXIT_FAILURE);
+  }
+
+  while (1) {
+    uint8_t recv_buf[1024];
+    ssize_t rlen = read(sockfd, recv_buf, sizeof(recv_buf));
+    if (rlen <= 0) {
+      printf("server closed connection or error. Exiting\n");
+      close(sockfd);
+      exit(EXIT_SUCCESS);
+    }
+
+    uint8_t msg_type = recv_buf[0];
+    if (msg_type == 1) {
+      printf("Recieved type 1 (shutdown) from server. Exiting\n");
+      fprintf(logfile, "Received type 1 from server. Exiting\n");
+      fclose(logfile);
+      close(sockfd);
+      exit(EXIT_SUCCESS);
+    } else if (msg_type == 0 && rlen >= 7) {
+      uint32_t sender_ip;
+      uint16_t sender_port;
+      memcpy(&sender_ip, recv_buf + 1, sizeof(sender_ip));
+      memcpy(&sender_port, recv_buf + 5, sizeof(sender_port));
+      struct in_addr ip_addr;
+      ip_addr.s_addr = sender_ip;
+      char *ip_str = inet_ntoa(ip_addr);
+      int port = ntohs(sender_port);
+
+      int msg_offset = 7;
+      int msg_len = rlen - msg_offset;
+      char message[1024];
+      memcpy(message, recv_buf + msg_offset, msg_len);
+      message[msg_len] = '\0';
+
+      printf("From %s:%d: %s", ip_str, port, message);
+      fprintf(logfile, "From %s:%d %s", ip_str, port, message);
+      fflush(logfile);
+    }
+   }*/
+
+  // close(sockfd);
+  return 0;
+}
---
+++ server.c
@@ -0,0 +1,208 @@
+#define _POSIX_C_SOURCE 200809L
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define MAX_CLIENTS 100
+
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s <port number> <# of clients>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  int port = atoi(argv[1]);
+  int max_clients = atoi(argv[2]);
+  if (max_clients > MAX_CLIENTS)
+    max_clients = MAX_CLIENTS;
+
+  int client_sockets[MAX_CLIENTS] = {0};
+  int server_fd, new_socket, sd, max_sd, activity;
+  struct sockaddr_in address;
+  int addrlen = sizeof(address);
+  fd_set readfds;
+
+  int client_finished[MAX_CLIENTS] = {0};
+  int total_connected_clients = 0;
+  server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd < 0) {
+    perror("socket failed");
+    exit(EXIT_FAILURE);
+  }
+
+  int opt = 1;
+
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+    perror("setsockopt");
+    exit(EXIT_FAILURE);
+  }
+
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(port);
+
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    perror("bind failure");
+    exit(EXIT_FAILURE);
+  }
+
+  if (listen(server_fd, max_clients) < 0) {
+    perror("listen");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Server is listening on port %d\n", port);
+
+  char leftover[MAX_CLIENTS][1024] = {{0}};
+  int leftover_len[MAX_CLIENTS] = {0};
+  while (1) {
+    FD_ZERO(&readfds);
+    FD_SET(server_fd, &readfds);
+    max_sd = server_fd;
+
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+      sd = client_sockets[i];
+      if (sd > 0)
+        FD_SET(sd, &readfds);
+      if (sd > max_sd)
+        max_sd = sd;
+    }
+
+    activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
+    if (activity < 0) {
+      perror("select error");
+      exit(EXIT_FAILURE);
+    }
+
+    if (FD_ISSET(server_fd, &readfds)) {
+      new_socket =
+          accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+
+      if (new_socket < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+      }
+
+      for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (client_sockets[i] == 0) {
+          client_sockets[i] = new_socket;
+          client_finished[i] = 0;
+          total_connected_clients++;
+          printf("Client added to slot %d, total: %d\n", i,
+                 total_connected_clients);
+          break;
+        }
+      }
+    }
+
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+      sd = client_sockets[i];
+      if (sd > 0 && FD_ISSET(sd, &readfds)) {
+        char buffer[1024];
+        int valread = read(sd, buffer, sizeof(buffer));
+        if (valread <= 0) {
+          close(sd);
+          client_sockets[i] = 0;
+          total_connected_clients--;
+          printf("Client disconnected from slot %d, total: %d\n", i,
+                 total_connected_clients);
+          continue;
+        }
+        char recvbuf[2048];
+        int rcvlen = leftover_len[i];
+        if (rcvlen > 0)
+          memcpy(recvbuf, leftover[i], rcvlen);
+        memcpy(recvbuf + rcvlen, buffer, valread);
+        rcvlen += valread;
+
+        int start = 0;
+        while (start < rcvlen) {
+          if (rcvlen - start < 1)
+            break;
+
+          uint8_t msg_type = (uint8_t)recvbuf[start];
+
+          if (msg_type == 0) {
+
+            int end = start + 1;
+            while (end < rcvlen && recvbuf[end] != '\n')
+              end++;
+            if (end >= rcvlen)
+              break;
+            int msg_data_len = end - (start + 1) + 1;
+
+            uint8_t out_buffer[1024];
+            size_t out_len = 0;
+            out_buffer[0] = 0;
+            out_len = 1;
+
+            struct sockaddr_in peer_addr;
+            socklen_t peer_addrlen = sizeof(peer_addr);
+            getpeername(sd, (struct sockaddr *)&peer_addr, &peer_addrlen);
+            uint32_t sender_ip = peer_addr.sin_addr.s_addr;
+            uint16_t sender_port = peer_addr.sin_port;
+            memcpy(out_buffer + out_len, &sender_ip, sizeof(sender_ip));
+            out_len += sizeof(sender_ip);
+            memcpy(out_buffer + out_len, &sender_port, sizeof(sender_port));
+            out_len += sizeof(sender_port);
+
+            memcpy(out_buffer + out_len, recvbuf + start + 1, msg_data_len);
+            out_len += msg_data_len;
+
+            for (int j = 0; j < max_clients; j++) {
+              int cli_sd = client_sockets[j];
+              if (cli_sd > 0) {
+                write(cli_sd, out_buffer, out_len);
+              }
+            }
+            start = end + 1;
+          } else if (msg_type == 1) {
+            client_finished[i] = 1;
+            printf("Client %d sent type 1\n", i);
+
+            int all_finished = 1;
+            int finished_count = 0;
+            for (int k = 0; k < max_clients; k++) {
+              if (client_sockets[k] > 0) {
+                if (client_finished[k] == 0) {
+                  all_finished = 0;
+                } else {
+                  finished_count++;
+                }
+              }
+            }
+            printf("Finished: %d / %d\n", finished_count,
+                   total_connected_clients);
+
+            if (all_finished && total_connected_clients > 0) {
+              printf("All clients finished. Terminating.\n");
+              uint8_t type1_msg = 1;
+              for (int k = 0; k < max_clients; k++) {
+                if (client_sockets[k] > 0) {
+                  write(client_sockets[k], &type1_msg, 1);
+                  close(client_sockets[k]);
+                }
+              }
+              close(server_fd);
+              exit(EXIT_SUCCESS);
+            }
+            start += 1;
+          } else {
+            start += 1;
+          }
+        }
+        if (start < rcvlen) {
+          leftover_len[i] = rcvlen - start;
+          memcpy(leftover[i], recvbuf + start, leftover_len[i]);
+        } else {
+          leftover_len[i] = 0;
+        }
+      }
+    }
+  }
+  return 0;
+}
